d9e1edbd6051676843daaaf997fed7840a7344e0
tavoxr <tavoxr23@gmail.com>
2021-07-13 00:39:55 -0600

    dashboard & product views

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,33 @@
+from accounts.models import Product
 from django.shortcuts import render
+from .models import *
 # Create your views here.
 
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    ordersTotal = orders.count()
+    ordersDelivered = Order.objects.filter(status = "Delivered").count()
+    ordersPending = Order.objects.filter(status = "Pending").count()
+    customers = Customer.objects.all()
+
+    context = {'orders': orders, 
+            'ordersTotal': ordersTotal, 
+            'ordersDelivered': ordersDelivered, 
+            'ordersPending': ordersPending,
+            'customers': customers,
+            }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+
+    products = Product.objects.all()
+    
+    
+    return render(request, 'accounts/products.html', {'products': products} )
 
 def customer(request):
     return render(request, 'accounts/customer.html')
--- crm/settings.py
+++ crm/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-crm',
+        'USER': 'postgres',
+        'PASSWORD': 'eva01',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
