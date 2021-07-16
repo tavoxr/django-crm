d6ce34b8e8e93c3ae9853df34641868d4c891125
tavoxr <tavoxr23@gmail.com>
2021-07-16 13:16:10 -0600

    email settings & templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
+from re import template
 from django.urls import path
 from .viewsets import *
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('register/', registerPage, name="register"),
@@ -15,6 +18,22 @@ urlpatterns = [
     path('create_order/<int:id_customer>/', createOrder, name="create_order"),
     path('edit_order/<int:id_order>/', editOrder, name="edit_order" ),
     path('delete_order/<int:id_order>/', deleteOrder, name="delete_order"),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name = "accounts/components/reset_password.html"), 
+         name="password_reset"
+         
+         ),
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(template_name= "accounts/components/reset_password_sent.html"), 
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name = "accounts/forms/password_reset_form.html"), 
+         name="password_reset_confirm"),
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view( template_name= "accounts/components/reset_password_complete.html"), 
+         name="password_reset_complete"),
+
 
      
 
--- crm/settings.py
+++ crm/settings.py
@@ -148,3 +148,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "tavoxdev@gmail.com"
+EMAIL_HOST_PASSWORD = "eva123qwsad"
